ccec1dfa882a6b04f576fe75eaedcb852d6325cd
Hannah N <nilagathp@gmail.com>
2019-06-14 05:09:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,19 +10,11 @@ planeteer_calls = %w[earth wind fire water heart]
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
-
 summon_captain_planet(planeteer_calls)
-
-# The planeteer_calls are supposed to be short, so we want to
-# find out if any of our calls have more than 4 characters.
-#
-# Use the any? method
-# http://ruby-doc.org/core-2.1.0/Enumerable.html#method-i-any-3F
 
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
-
 long_planeteer_calls(planeteer_calls)
 
 # Help Monterey Jack find the cheese!
