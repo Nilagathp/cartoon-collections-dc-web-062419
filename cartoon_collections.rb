faed1b2ba10a08f9320a9ad73c0a8d912c038db5
Hannah N <nilagathp@gmail.com>
2019-06-14 05:05:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,12 @@
+dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
 def roll_call_dwarves(array)
   array.each_with_index do |dwarf, index|
     puts "{index + 1}. #{dwarf}"
