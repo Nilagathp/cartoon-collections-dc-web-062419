f1d3d9899129ab263ecc4b82b454ab99b0d978bc
Hannah N <nilagathp@gmail.com>
2019-06-14 05:03:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,6 @@
 def roll_call_dwarves(array)
   array.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
-  #i = 1
-  #array.each_with_index do |names|
-    #puts "#{i}. #{names}"
-    #i += 1
   end
 
 def summon_captain_planet# code an argument here
